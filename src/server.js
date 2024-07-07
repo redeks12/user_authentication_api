8778072154e4c3bf9cbf945e9c5974d8078d319a
kumdan job <redeks123456@gmail.com>
2024-07-07 23:14:57 +0100

    completed api push for

--- src/server.js
+++ src/server.js
@@ -6,8 +6,11 @@ import bodyParser from "body-parser";
 import Users from "./routes/users.js";
 import Organisations from "./routes/organisations.js";
 import Auth from "./routes/auth.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const port = 3000;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
