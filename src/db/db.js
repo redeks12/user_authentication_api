e3e26198fdc959413d3b3663b28ef7f7e687b573
kumdan job <redeks123456@gmail.com>
2024-07-08 00:15:29 +0100

    completed api push for

--- src/db/db.js
+++ src/db/db.js
@@ -2,6 +2,18 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import process from "process";
 dotenv.config();
-const db = new Sequelize(process.env.DB_ADDR, { logging: false });
+
+let db;
+console.log(process.env.DB_ADDR);
+if (process.env.DB_ADDR) {
+  db = new Sequelize(process.env.DB_ADDR, { logging: false });
+} else {
+  db = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database.db",
+    logging: false,
+  });
+}
+// db = new Sequelize(process.env.DB_ADDR, { logging: false });
 
 export default db;
